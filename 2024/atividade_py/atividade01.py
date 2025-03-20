3123079a128f62b25e76d3ca620e7858120bd792
Guilherme Maia <130709532+GuilhermeMaiaBezerra@users.noreply.github.com>
2025-03-19 23:43:51 -0400

    Delete 2024/atividade_py directory

--- 2024/atividade_py/atividade01.py
+++
@@ -1,8 +0,0 @@
-print("Informe um número: ",end="");
-
-num = int(input())
-
-for i in range(1,11):
- resultado = num*i;
- print(f"{num} x {i} = {resultado}");
- i+=1;
--- 2024/atividade_py/atividade07.py
+++
@@ -1,9 +0,0 @@
-contador = 0;
-
-for i in range(1,11):
-    print(f"Informe o {i}º número: ",end="");
-    num = int(input());
-    if num >= 30 and num <= 90:
-        contador += 1;
-
-print(f"Quantidade de numeros entre 30 e 90: {contador}");
